f5bcb2490c51f844c0c77ea5af1d9abb3349a717
liuzhihaoa <liu413780791@qq.com>
2023-04-20 14:44:52 +0800

    修改request的引入路径

--- vue3/api/user/index.ts
+++ vue3/api/user/index.ts
@@ -3,10 +3,10 @@
  * @email: liuzhihao@hatech.com.cn
  * @Date: 2023-04-04 14:00:06
  * @LastEditors: liuzhihao
- * @LastEditTime: 2023-04-18 16:00:40
+ * @LastEditTime: 2023-04-20 11:32:11
  * @description: 描述
  */
-import { http } from "../../request";
+import { http } from "@/request";
 import { menuData, menuRes, loginData, loginRes } from "./type";
 export const getMenuData = async (params: menuData) => {
   const { code, data, message } = await http.request<menuRes>("/rbac/authority-group/list/user", "get", params);
@@ -24,7 +24,7 @@ export const getMenuData = async (params: menuData) => {
 };
 
 export const getUserInfo = async (params: loginData) => {
-  const { code, data, message } = await http.post<loginRes>("/oauth/user/login", params, { timeout: 30000,loading:false });
+  const { code, data, message } = await http.post<loginRes>("/oauth/user/login", params, { timeout: 30000, loading: true });
   if (code === 200 && data) {
     return {
       success: true,
