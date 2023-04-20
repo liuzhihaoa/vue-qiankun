e93c3d3463e284120254c82868bb8ba0af9da789
liuzhihaoa <liu413780791@qq.com>
2023-04-20 10:18:13 +0800

    修改请求的调用方式

--- vue3/api/user/index.ts
+++ vue3/api/user/index.ts
@@ -3,18 +3,13 @@
  * @email: liuzhihao@hatech.com.cn
  * @Date: 2023-04-04 14:00:06
  * @LastEditors: liuzhihao
- * @LastEditTime: 2023-04-10 11:46:40
+ * @LastEditTime: 2023-04-18 16:00:40
  * @description: 描述
  */
 import { http } from "../../request";
 import { menuData, menuRes, loginData, loginRes } from "./type";
 export const getMenuData = async (params: menuData) => {
-  const { code, data, message } = await http.request<menuRes>({
-    url: "/rbac/authority-group/list/user",
-    method: "get",
-    params: params,
-    
-  });
+  const { code, data, message } = await http.request<menuRes>("/rbac/authority-group/list/user", "get", params);
   // const { code, data, message } = await http.get<menuRes>("/rbac/authority-group/list/user", {
   //   params: params,
   // });
@@ -29,7 +24,7 @@ export const getMenuData = async (params: menuData) => {
 };
 
 export const getUserInfo = async (params: loginData) => {
-  const { code, data, message } = await http.post<loginRes>("/oauth/user/login", params, { timeout: 30000 });
+  const { code, data, message } = await http.post<loginRes>("/oauth/user/login", params, { timeout: 30000,loading:false });
   if (code === 200 && data) {
     return {
       success: true,
